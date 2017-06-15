67d32856ae7e71c499d558f61e97898c6cb8559e
Benjamin <benjamin.christau@oyez.fr>
2017-06-15 16:31:39 +0200

    Workign parse - Clean and promisify all the process

--- src/index.js
+++ src/index.js
@@ -24,6 +24,10 @@ const mysqlConnection = mysql.createConnection({
 // Function declaration
 function initMySQL() {
     return new Promise((resolve, reject) => {
+        if (process.env.DEBUG === "true") {
+            Log.info(logTags, "Running 'initMySQL'...");
+        }
+
         mysqlConnection.connect((err) => {
             if (err) {
                 reject({ message: "Error with MySQL connection", error: err });
@@ -38,7 +42,11 @@ function initMySQL() {
 
 function getNewsURL() {
     return new Promise((resolve, reject) => {
-        const query = "SELECT `sagas`.`title`, `sagas`.`newsUrl` FROM `sagas`";
+        if (process.env.DEBUG === "true") {
+            Log.info(logTags, "Running 'getNewsURL'...");
+        }
+
+        const query = "SELECT `sagas`.`id`, `sagas`.`title`, `sagas`.`newsUrl` FROM `sagas`";
         mysqlConnection.query(query, [], (err, rows) => {
             if (err) {
                 reject({ message: "Error with MySQL.", error: err });
@@ -55,18 +63,30 @@ function getNewsURL() {
 
 function getNews(res) {
     return new Promise((resolve, reject) => {
+        if (process.env.DEBUG === "true") {
+            Log.info(logTags, "Running 'getNews'...");
+        }
         if (res && typeof (res) === "object" && res.length !== 0) {
+            const fetches = [];
             for (let i = 0; i < res.rows.length; i++) {
                 const saga = res.rows[i];
-                fetch(saga.newsUrl)
-                    .then(fetchRes => fetchRes.text())
-                    .then(xmlData => XMLParser.parse(xmlData))
-                    .catch((err) => {
-                        Log.err(logTags, `Error on news fetch, ${err}`);
-                        reject();
-                    });
+                fetches.push(fetch(saga.newsUrl)
+                    .then(fetchesRes => fetchesRes.text())
+                    .then(xmlData => ({ saga, xmlData }))
+                    .catch(err => reject({ err: `Error on news fetch, ${err}` }))
+                );
             }
+
+            Promise.all(fetches)
+                .then(fetchRes => resolve(fetchRes))
+                .catch(err => reject({ err: `Error on news fetch, ${err}` }));
         }
+    });
+}
+
+function postNews() {
+    return new Promise((resolve) => {
+        resolve();
     });
 }
 
@@ -75,7 +95,28 @@ function getNews(res) {
 initMySQL()
     .then(getNewsURL)
     .then(getNews)
+    .then(getNewsRes => new Promise((resolve, reject) => {
+        const parses = [];
+        for (let i = 0; i < getNewsRes.length; i++) {
+            parses.push(XMLParser.parse(getNewsRes[i].saga, getNewsRes[i].xmlData)
+                .then((parseRes) => {
+                    Log.info(logTags, `${parseRes.newsParsed} news has successfully been parsed for the saga '${getNewsRes[i].saga.title}'.`);
+                    return parseRes;
+                })
+            );
+        }
+
+        Promise.all(parses)
+            .then((parsesRes) => {
+                const totalNewsParsed = parsesRes.reduce((a, b) => a + parseInt(b.newsParsed, 10), 0);
+                Log.info(logTags, `A total of ${totalNewsParsed} has been parsed.`);
+            })
+            .catch(err => reject({ err: `Error on news parse, ${err}` }));
+    }))
+    .then(postNews)
     .catch((err) => {
         Log.err(logTags, "Error when setting up the service.");
+        Log.disableTimestamp();
         Log.err(err);
+        Log.enableTimestamp();
     });
--- src/xmlparser.js
+++ src/xmlparser.js
@@ -1,24 +1,105 @@
 //////////
 // Lib imports
 import xml2js from "xml2js";
+import { JSDOM } from "jsdom";
 import Log from "sagasphere_logger";
 
 const logTags = ["SagaSphere", "News", "XMLParser"];
 
-function parse(xmlString) {
-    const parser = new xml2js.Parser();
+function rssFieldToString(sagaId, rssField) {
+    let parsedRssField = typeof (rssField) !== "undefined" ? rssField : "";
 
-    return parser.parseString(xmlString, (err, result) => {
-        if (err) {
-            Log.error(logTags, `Error when parsing the XML : ${err}`);
+    if (Array.isArray(parsedRssField) && parsedRssField.length > 0 && parsedRssField[0]) {
+        parsedRssField = parsedRssField.join();
+    }
+
+    // TODO
+    // Specific parsing
+    switch (sagaId) {
+        case 1: // Donjon de Naheulbeuk
+            break;
+        case 2: // Reflet d'Acide
+            parsedRssField = parsedRssField.replace(/&#(\d+);/g, (match, HTMLentity) => String.fromCharCode(HTMLentity));
+            break;
+        default:
+    }
+
+    return parsedRssField;
+}
+
+function parseDesc(sagaId, desc) {
+    if (process.env.DEBUG === "true") {
+        Log.info(logTags, "Running 'parseDesc'...");
+    }
+
+    let parsedDesc = typeof (desc) !== "undefined" ? desc : "";
+    let matchs = null;
+
+    // Specific parsing
+    switch (sagaId) {
+        case 1: // Donjon de Naheulbeuk
+            break;
+        case 2: // Reflet d'Acide
+            matchs = parsedDesc.match(/<p>(.*?)<\/p>/gi);
+            if (Array.isArray(matchs) && matchs.length > 0 && matchs[0]) {
+                parsedDesc = JSDOM.fragment(matchs[0]).textContent;
+            }
+            break;
+        default:
+    }
+
+    return parsedDesc;
+}
+
+function parseNews(saga, news) {
+    return new Promise((resolve, reject) => {
+        if (process.env.DEBUG === "true") {
+            Log.info(logTags, "Running 'parseNews'...");
+        }
+
+        const newsParsed = {};
+        if (news.title && news.pubDate) {
+            newsParsed.saga = saga.id;
+            newsParsed.link = rssFieldToString(saga.id, news.link) !== "" ? rssFieldToString(saga.id, news.link) : saga.url;
+            newsParsed.title = rssFieldToString(saga.id, news.title);
+            newsParsed.date = rssFieldToString(saga.id, news.pubDate);
+            newsParsed.description = parseDesc(saga.id, rssFieldToString(saga.id, news.description));
+
+            resolve({ news: newsParsed });
         }
         else {
-            // TODO add and use result parameter
-            console.log(result);
+            reject({ err: `Can't save the news for the saga "${saga.title}", missing title or publication date` });
         }
     });
 }
 
+function parseFeed(saga, xmlString) {
+    return new Promise((resolve, reject) => {
+        if (process.env.DEBUG === "true") {
+            Log.info(logTags, "Running 'parseFeed'...");
+        }
+
+        const parser = new xml2js.Parser();
+        parser.parseString(xmlString, (xmlErr, result) => {
+            if (xmlErr) {
+                reject({ err: `Error when parsing the XML for "${saga.title}" : ${xmlErr}` });
+            }
+            else if (result.rss && result.rss.channel && result.rss.channel[0] && result.rss.channel[0].item) {
+                const items = result.rss.channel[0].item;
+                const promises = [];
+
+                for (let i = 0; i < items.length; i++) {
+                    promises.push(parseNews(saga, items[i]));
+                }
+
+                Promise.all(promises)
+                    .then(resolve({ newsParsed: promises.length }))
+                    .catch(parseErr => reject({ err: `Error when parsing one news from "${saga.title}", ${parseErr}` }));
+            }
+        });
+    });
+}
+
 export default {
-    parse: xmlString => parse(xmlString)
+    parse: (saga, xmlString) => parseFeed(saga, xmlString)
 };
